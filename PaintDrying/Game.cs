22ecdfc900926c2c0a7f3cdb81769936da7adde0
dotpep <pepelnine@gmail.com>
2024-01-28 05:15:59 +0600

    Implement changing color text with arrow choise in CLI

--- PaintDrying/Game.cs
+++ PaintDrying/Game.cs
@@ -72,7 +72,36 @@ Welcome to the Pait Drying Simulator. What would you like to do?
 
         private void RunFirstChoise()
         {
-            WriteLine("Placeholder for 1st choice!");
+            string prompt = "What color paint would you like to watch dry?";
+            string[] options = { "Red", "Green", "Blue", "Yellow" };
+
+            Menu colorMenu = new Menu(prompt, options);
+            int selectedIndex = colorMenu.Run();
+
+            BackgroundColor = ConsoleColor.White;
+            switch (selectedIndex)
+            {
+                case 0:
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("\nHere is some riveting red paint drying for you...");
+                    break;
+                case 1:
+                    ForegroundColor = ConsoleColor.Green;
+                    WriteLine("\nHere is some riveting green paint drying for you...");
+                    break;
+                case 2:
+                    ForegroundColor = ConsoleColor.Blue;
+                    WriteLine("\nHere is some riveting blue paint drying for you...");
+                    break;
+                case 3:
+                    ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine("\nHere is some riveting yellow paint drying for you...");
+                    break;
+            }
+            ResetColor();
+
+            WriteLine("That's all for this game demo!");
+
             ExitGame();
         }
     }
