a9b046d72d49f69c2c9e532122a83cd52000779e
dotpep <pepelnine@gmail.com>
2024-01-28 05:01:26 +0600

    Implement cycling through menu options by pressing enter

--- PaintDrying/Game.cs
+++ PaintDrying/Game.cs
@@ -13,28 +13,67 @@ namespace PaintDrying
     {
         public void Start()
         {
-            //WriteLine("The game is starting.");
-            //ConsoleKeyInfo keyPressed = ReadKey();
-            //if (keyPressed.Key == ConsoleKey.Enter)
-            //{
-            //    WriteLine("Enter pressed");
-            //}
-            //else if (keyPressed.Key == ConsoleKey.UpArrow)
-            //{
-            //    WriteLine("UpArrow pressed");
-            //}
-            //else
-            //{
-            //    WriteLine("Not enter");
-            //}
+            Title = "Paint Drying - The Game!";
+            RunMainMenu();
 
-            string prompt = "Welcome to the Pait Drying Simulator. What would you like to do?";
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+
+        private void RunMainMenu()
+        {
+            string prompt = @"
+ ██▓███   ▄▄▄       ██▓ ███▄    █ ▄▄▄█████▓   ▓█████▄  ██▀███ ▓██   ██▓ ██▓ ███▄    █   ▄████ 
+▓██░  ██▒▒████▄    ▓██▒ ██ ▀█   █ ▓  ██▒ ▓▒   ▒██▀ ██▌▓██ ▒ ██▒▒██  ██▒▓██▒ ██ ▀█   █  ██▒ ▀█▒
+▓██░ ██▓▒▒██  ▀█▄  ▒██▒▓██  ▀█ ██▒▒ ▓██░ ▒░   ░██   █▌▓██ ░▄█ ▒ ▒██ ██░▒██▒▓██  ▀█ ██▒▒██░▄▄▄░
+▒██▄█▓▒ ▒░██▄▄▄▄██ ░██░▓██▒  ▐▌██▒░ ▓██▓ ░    ░▓█▄   ▌▒██▀▀█▄   ░ ▐██▓░░██░▓██▒  ▐▌██▒░▓█  ██▓
+▒██▒ ░  ░ ▓█   ▓██▒░██░▒██░   ▓██░  ▒██▒ ░    ░▒████▓ ░██▓ ▒██▒ ░ ██▒▓░░██░▒██░   ▓██░░▒▓███▀▒
+▒▓▒░ ░  ░ ▒▒   ▓▒█░░▓  ░ ▒░   ▒ ▒   ▒ ░░       ▒▒▓  ▒ ░ ▒▓ ░▒▓░  ██▒▒▒ ░▓  ░ ▒░   ▒ ▒  ░▒   ▒ 
+░▒ ░       ▒   ▒▒ ░ ▒ ░░ ░░   ░ ▒░    ░        ░ ▒  ▒   ░▒ ░ ▒░▓██ ░▒░  ▒ ░░ ░░   ░ ▒░  ░   ░ 
+░░         ░   ▒    ▒ ░   ░   ░ ░   ░          ░ ░  ░   ░░   ░ ▒ ▒ ░░   ▒ ░   ░   ░ ░ ░ ░   ░ 
+               ░  ░ ░           ░                ░       ░     ░ ░      ░           ░       ░ 
+                                               ░               ░ ░                            
+Welcome to the Pait Drying Simulator. What would you like to do?
+(Use the arrow keys to cycle through options and press enter to select an option.)";
             string[] options = { "Play", "About", "Exit" };
             Menu mainMenu = new Menu(prompt, options);
             int selectedIndex = mainMenu.Run();
 
-            WriteLine("Press any key to exit...");
+            switch (selectedIndex)
+            {
+                case 0:
+                    RunFirstChoise();
+                    break;
+                case 1:
+                    DisplayAboutInfo();
+                    break;
+                case 2:
+                    ExitGame();
+                    break;
+            }
+        }
+
+        private void ExitGame()
+        {
+            WriteLine("\nPress any key to exit...");
             ReadKey(true);
+            Environment.Exit(0);
+        }
+
+        private void DisplayAboutInfo()
+        {
+            Clear();
+            WriteLine("someting about About Game information.");
+
+            WriteLine("Press any key to return to the menu.");
+            ReadKey(true);
+            RunMainMenu();
+        }
+
+        private void RunFirstChoise()
+        {
+            WriteLine("Placeholder for 1st choice!");
+            ExitGame();
         }
     }
 }
