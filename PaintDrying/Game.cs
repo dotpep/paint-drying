04c806734be712d929af25f295b95a86f8489eb9
dotpep <pepelnine@gmail.com>
2024-01-28 04:29:46 +0600

    Defining basic structure of Console Keyboard-Controller Menu

---
+++ PaintDrying/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+
+namespace PaintDrying
+{
+    internal class Game
+    {
+        public void Start()
+        {
+            //WriteLine("The game is starting.");
+            //ConsoleKeyInfo keyPressed = ReadKey();
+            //if (keyPressed.Key == ConsoleKey.Enter)
+            //{
+            //    WriteLine("Enter pressed");
+            //}
+            //else if (keyPressed.Key == ConsoleKey.UpArrow)
+            //{
+            //    WriteLine("UpArrow pressed");
+            //}
+            //else
+            //{
+            //    WriteLine("Not enter");
+            //}
+
+            string prompt = "Welcome to the Pait Drying Simulator. What would you like to do?";
+            string[] options = { "Play", "About", "Exit" };
+            Menu mainMenu = new Menu(prompt, options);
+            mainMenu.DisplayOptions();
+
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+    }
+}
--- PaintDrying/Program.cs
+++ PaintDrying/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using static System.Console;
+
+namespace PaintDrying;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Game myGame = new Game();
+        myGame.Start();
+    }
+}
